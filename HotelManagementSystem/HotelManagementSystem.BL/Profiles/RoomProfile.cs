0705a1541a7f8294fac383bdbb36146935cde88e
Mehemmed476 <mahammadax-ab205@code.edu.az>
2025-07-01 13:27:19 +0400

    Added Room Crud

---
+++ HotelManagementSystem/HotelManagementSystem.BL/Profiles/RoomProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using HotelManagementSystem.BL.DTOs.Room;
+using HotelManagementSystem.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem.BL.Profiles;
+
+public class RoomProfile : Profile
+{
+    public RoomProfile()
+    {
+        CreateMap<AddRoomDTO, Room>().ReverseMap();
+        CreateMap<GetRoomDTO, Room>().ReverseMap();
+        CreateMap<UpdateRoomDTO, Room>().ReverseMap();
+    }
+}
