2d9c572184b2c1038785e34b7e898f7fb2756d15
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2025-07-01 12:49:38 +0400

    Added Service CRUD

---
+++ HotelManagementSystem/HotelManagementSystem.BL/Services/Abstractions/IServiceService.cs
@@ -0,0 +1,19 @@
+﻿using HotelManagementSystem.BL.DTOs.ServiceDTO;
+using HotelManagementSystem.Core.Entities;
+using System.Linq.Expressions;
+
+namespace HotelManagementSystem.BL.Services.Abstractions;
+
+public interface IServiceService
+{
+    public Task AddServiceAsync(AddServiceDTO addServiceDTO);
+    public Task<ICollection<GetServiceDTO>> GetAllServices();
+    public Task<GetServiceDTO> GetServiceById(Guid Id);
+    public Task<GetServiceDTO> GetServiceByCondition(Expression<Func<Service, bool>> expression);
+    public Task HardDelete(Guid Id);
+    public Task Update(Guid Id, UpdateServiceDTO updateServiceDTO);
+    public Task SoftDelete(Guid Id);
+    public Task RevertSoftDelete(Guid Id);
+    public Task<int> SaveChangesAsync();
+
+}
---
+++ HotelManagementSystem/HotelManagementSystem.DL/Exceptions/BaseException.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelManagementSystem.DL.Exceptions;
+
+public class BaseException : Exception
+{
+    public BaseException() : base("Something went wrong.")
+    {
+
+    }
+    public BaseException(string mes) : base(mes)
+    {
+
+    }
+}
